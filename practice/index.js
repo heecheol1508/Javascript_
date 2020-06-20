218cdaf062b6fb1c4cc72596d8753c6d8fd934bd
TIL <52685275+heecheol1508@users.noreply.github.com>
2020-06-20 14:24:27 +0900

    classList.toggle()

--- practice/index.js
+++ practice/index.js
@@ -3,12 +3,13 @@ const title = document.getElementById("title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const hasClass = title.classList.contains(CLICKED_CLASS);
-  if (hasClass) {
-    title.classList.remove(CLICKED_CLASS);
-  } else {
-    title.classList.add(CLICKED_CLASS);
-  }
+  //   const hasClass = title.classList.contains(CLICKED_CLASS);
+  //   if (hasClass) {
+  //     title.classList.remove(CLICKED_CLASS);
+  //   } else {
+  //     title.classList.add(CLICKED_CLASS);
+  //   }
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
