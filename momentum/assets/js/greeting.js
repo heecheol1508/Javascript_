ca1fc9db4c07218f69da155770b54dbc25a1eda6
TIL <52685275+heecheol1508@users.noreply.github.com>
2020-06-25 17:37:33 +0900

    delete todos

--- momentum/assets/js/greeting.js
+++ momentum/assets/js/greeting.js
@@ -6,7 +6,7 @@ const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
 function saveName(text) {
-  localStorage.setItem(USER_LS, tesxt);
+  localStorage.setItem(USER_LS, text);
 }
 
 function handleSubmit(event) {
--- momentum/assets/js/todo.js
+++ momentum/assets/js/todo.js
@@ -1,4 +1,72 @@
-const todoForm = document.querySelector(".js-todoForm");
-const todoInput = todoForm.querySelector("input");
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
 
+const TODOS_LS = "toDos";
 
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+// 자바스크립트는 Localstorage에 있는 모든 데이터를 string으로 저장한다
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const newId = toDos.length + 1;
+  const toDoText = document.createElement("span");
+  toDoText.innerText = text;
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "x";
+  delBtn.addEventListener("click", deleteToDo);
+  li.id = newId;
+  li.appendChild(toDoText);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function something(toDo) {
+  console.log(toDo.text);
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    // parsedToDos.forEach(something);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
